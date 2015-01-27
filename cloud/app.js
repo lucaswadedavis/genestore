a2ba5c413c53d5bb285a5288a5f053235d5e5d8e
Luke Davis <lukefable@gmail.com>
2015-01-27 20:30:17 +0000

    copy of the awesome defiant light project

---
+++ cloud/app.js
@@ -0,0 +1,20 @@
+var creds = require('cloud-creds');
+var express = require('express');
+
+var app = express();
+app.use(express.bodyParser());
+app.post('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
+  res.json({ message: req.body.message });  // Send a JSON response
+});
+
+app.get('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
+  res.send("success"); // Send a JSON response
+});
+
+/* 
+app.get('/cburl', function(req, res){
+  res.send(200);
+});
+*/
+
+app.listen();
---
+++ cloud/main.js
@@ -0,0 +1,41 @@
+var creds = require('cloud/creds.js');
+var fauxPoe = require('cloud/faux-poe.js');
+var Mandrill = require('mandrill');
+Mandrill.initialize(creds.mandrill );
+
+Parse.Cloud.job("printCreds", function(request, response){
+  console.log(creds);
+});
+
+Parse.Cloud.job("sendMail",function(request,response){
+  var manyPoems=[];
+  for (var i=0;i<100;i++){
+    manyPoems.push(fauxPoe() );
+  }
+  
+  Mandrill.sendEmail({
+    message: {
+      text: manyPoems.join("\n\n"),
+      subject: "Faux Poe",
+      from_email: "parse@cloudcode.com",
+      from_name: "Faux Poe",
+      to: [
+        {
+          email: "lucaswadedavis@gmail.com",
+          name: "Luke"
+        }
+      ]
+    },
+    async: true
+  },{
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      response.success("Email sent!");
+    },
+    error: function(httpResponse) {
+      console.error(httpResponse);
+      response.error("Uh oh, something went wrong");
+    }
+  });
+
+});
